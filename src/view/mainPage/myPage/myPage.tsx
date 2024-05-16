15636b9bf870cc74bcdc11a5a45c7b9783d58e20
MinjoonHK <minjoon.park.hk@gmail.com>
2024-05-16 13:27:39 +0900

    resumeList and myPage added

--- src/view/mainPage/myPage/myPage.tsx
+++ src/view/mainPage/myPage/myPage.tsx
@@ -1,5 +1,42 @@
+import { Button } from "antd";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+
+interface DecodedToken {
+  category: string;
+  exp: number;
+  iat: number;
+  role: string;
+  uNum: number;
+  username: string;
+}
+
 const MyPage = () => {
-  return <div>마이 페이지 입니다</div>;
+  return (
+    <div className="mypageWrapper" style={{ padding: "5% 10%" }}>
+      <div
+        className="mypageInnerWrapper"
+        style={{ backgroundColor: "rgb(220,220,220)" }}
+      >
+        gd
+      </div>
+      <Button
+        onClick={() => {
+          let accessToken = localStorage.getItem("access") ?? "";
+          let Decoded: DecodedToken = jwtDecode(accessToken);
+          let res = axios
+            .post("/user/delete", {
+              unum: Decoded.uNum,
+            })
+            .then((res) => {
+              console.log(res);
+            });
+        }}
+      >
+        회원 탈퇴
+      </Button>
+    </div>
+  );
 };
 
 export default MyPage;
--- src/view/mainPage/resumeList/resumeList.tsx
+++ src/view/mainPage/resumeList/resumeList.tsx
@@ -1,19 +1,154 @@
 import axiosInstance from "@/api/api";
-import { Button, Form } from "antd";
+import { Button, Form, Pagination } from "antd";
+import "./resumeList.css";
+import { EyeOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import Search from "antd/es/input/Search";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
-import { Cookies } from "react-cookie";
+interface ResumeList {
+  num: number;
+  r_num: number;
+  rating: number;
+  rating_count: number;
+  read_num: number;
+  title: string;
+  w_date: string;
+}
 const ResumeList = () => {
-  const cookies = new Cookies();
+  const [resumeList, setResumeList] = useState<ResumeList[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  async function fetchSearch(search: string) {
+    let res = await axiosInstance
+      .get("/board/list/search", {
+        params: {
+          page: 0,
+          keyword: search,
+        },
+      })
+      .then((res) => {
+        setResumeList(res.data.response);
+        setTotalPages(res.data.totalPages);
+      });
+  }
+  async function fetchList(page: number) {
+    let res = await axiosInstance
+      .get("/board/list", {
+        params: {
+          page: page,
+        },
+      })
+      .then((res) => {
+        setResumeList(res.data.response);
+        setTotalPages(res.data.totalPages);
+      });
+  }
+  useEffect(() => {
+    fetchList(0);
+  }, []);
   return (
     <div>
-      <Button
-        onClick={async () => {
-          let response = await axiosInstance.get("/api/v1/chatGpt/modelList");
-        }}
+      <div
+        className="search"
+        style={{ display: "flex", justifyContent: "center", paddingTop: "3%" }}
       >
-        TEST
-      </Button>
+        <Search
+          onSearch={(value) => {
+            fetchSearch(value);
+          }}
+          size="large"
+          placeholder="검색어를 입력하세요"
+          style={{ width: "40%" }}
+        />
+      </div>
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "3% 0" }}
+      >
+        <Pagination
+          onChange={(page) => {
+            fetchList(page);
+          }}
+          pageSize={5}
+          showSizeChanger={false}
+          total={totalPages * 5 - 5}
+        />
+      </div>
+      <div className="wrapper">
+        <div
+          className="innerWrapper"
+          style={{
+            display: "flex",
+            width: "100%",
+            flexDirection: "column",
+          }}
+        >
+          {resumeList.map((resume, index) => {
+            return (
+              <div
+                key={`resume.r_num${index}`}
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <div
+                  className="contentBox"
+                  style={{
+                    width: "50%",
+                    border: "1px solid rgb(225,225,225)",
+                    padding: "3% 5%",
+                    margin: "1% 0",
+                  }}
+                >
+                  <div className="contentTitle">
+                    <h3>
+                      <Link
+                        style={{ color: "black" }}
+                        to={`/main/resumelist/${resume.r_num}`}
+                      >
+                        {resume.title}
+                      </Link>
+                    </h3>
+                  </div>
+                  <div
+                    className="contentMainText"
+                    style={{
+                      display: "-webkit-box",
+                      WebkitBoxOrient: "vertical",
+                      margin: "5% 0",
+                      fontSize: "16px",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      WebkitLineClamp: 2,
+                      width: "100%",
+                    }}
+                  >
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Obcaecati asperiores explicabo facere repudiandae!
+                    Obcaecati, eius et dolorum nulla recusandae asperiores
+                    sapiente ratione magni ad, cum amet qui dolorem pariatur
+                    dolor. Lorem ipsum dolor, sit amet consectetur adipisicing
+                    elit. Repellendus dolorum repellat rerum aut ab esse,
+                    accusantium illum, hic culpa repudiandae nam! Quas nam
+                    delectus vitae modi ut veniam ea reprehenderit.
+                  </div>
+                  <div
+                    className="contentFooter"
+                    style={{
+                      justifyContent: "space-between",
+                      display: "flex",
+                      fontSize: "14px",
+                    }}
+                  >
+                    <div className="contentDate">{resume.w_date}</div>
+                    <div className="contentView">
+                      <EyeOutlined /> {resume.read_num}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
